5c30a8ad36da566b8e8d0fc60c60a9cf3e9d63b9
Manmeet Singh Parmar <manietdavv@gmail.com>
2021-01-05 15:51:02 +0530

    Add files via upload

---
+++ js-exercises/duplicateLetters.js
@@ -0,0 +1,25 @@
+
+function duplicateLetters(...args) {
+  const hashObj = {};
+  const arr = args[0].split('');
+  let maxFrequency = -Infinity;
+  // for (const element of arr) {
+  //   if(hashObj.hasOwnProperty(element))
+  //      hashObj[element] += 1;
+  //   else
+  //       hashObj[element] = 1;
+  //   maxFrequency = Math.max(maxFrequency , hashObj[element]);
+  // }
+  arr.forEach(element => {
+    if(hashObj.hasOwnProperty(element))
+       hashObj[element] += 1;
+    else
+        hashObj[element] = 1;
+    maxFrequency = Math.max(maxFrequency , hashObj[element]);
+  });
+  return ( maxFrequency > 1 ) ? maxFrequency : false;
+}
+
+export {
+  duplicateLetters
+}
