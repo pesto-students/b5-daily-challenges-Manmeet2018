81fa5c126b16d0f86a39356ccaa905638755f8ee
Manmeet2018 <manietdavv@gmail.com>
2021-01-05 18:08:49 +0530

    fix: consider the equality case

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,7 +1,7 @@
 function isTriangle(...args) {
-  return args[0] + args[1] > args[2]
-      && args[1] + args[2] > args[0]
-      && args[2] + args[0] > args[1];
+  return args[0] + args[1] >= args[2]
+      && args[1] + args[2] >= args[0]
+      && args[2] + args[0] >= args[1];
 }
 
 export {
