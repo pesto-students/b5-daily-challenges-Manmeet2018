a0054ddbbb0e136cf0442570a138eb386b228e7f
Manmeet2018 <manietdavv@gmail.com>
2021-01-14 13:46:29 +0530

    feat: added SumPrimes

---
+++ js-exercises/sum-primes/sumPrimes.js
@@ -0,0 +1,37 @@
+const isNumber = (num) => {
+  if (typeof num !== 'number') {
+    throw new TypeError(`TypeError: ${num} is not a number`);
+  }
+};
+
+const isPrime = (num) => {
+  const numSqrt = Math.sqrt(num);
+  for (let i = 2; i <= numSqrt; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+function* generatePrimeNumbers(number) {
+  let i = 2;
+  while (true) {
+    if (i > number) break;
+    if (isPrime(i)) {
+      yield i;
+    }
+    i += 1;
+  }
+}
+function sumPrimes(numberUpto) {
+  isNumber(numberUpto);
+  let PrimeSum = 0;
+  const iter = generatePrimeNumbers(numberUpto);
+  for (const el of iter) {
+    PrimeSum += el;
+  }
+  return PrimeSum;
+}
+
+export {
+  sumPrimes,
+};
